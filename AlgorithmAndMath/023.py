28d642dbdb01d7e79697fcfa7d75cdd9234c587e
Hiraku Onuma <hiraku00@gmail.com>
2024-08-04 14:12:38 +0900

    add 023

---
+++ AlgorithmAndMath/023.py
@@ -0,0 +1,8 @@
+def main():
+    N = int(input())
+    B = list(map(int, input().split()))
+    R = list(map(int, input().split()))
+    print((sum(B)+sum(R))/N)
+
+if __name__ == "__main__":
+    main()
