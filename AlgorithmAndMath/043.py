61fd1e0d80fdc744a1b75c695f2619b182d8465a
Hiraku Onuma <hiraku00@gmail.com>
2024-08-22 21:44:01 +0900

    add 043

---
+++ AlgorithmAndMath/043.py
@@ -0,0 +1,28 @@
+def is_connected(N, edges):
+    graph = [[] for _ in range(N)]
+    for a, b in edges:
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)
+    return dfs(graph, N)
+
+def dfs(graph, N):
+    visited = [False] * N
+    stack = [0]
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            visited[v] = True
+            for nv in graph[v]:
+                stack.append(nv)
+    return all(visited)
+
+def main():
+    N, M = map(int, input().split())
+    edges = [list(map(int, input().split())) for _ in range(M)]
+    if is_connected(N, edges):
+        print("The graph is connected.")
+    else:
+        print("The graph is not connected.")
+
+if __name__ == "__main__":
+    main()
