9bc759f15d9e1db264c667627cbcf436bb8e8295
Hiraku Onuma <hiraku00@gmail.com>
2024-09-13 13:56:34 +0900

    add 026, 027(2nd)

---
+++ AlgorithmAndMath/2nd/026.py
@@ -0,0 +1,12 @@
+def calc_expectation(N):
+    res = 0
+    for i in range(N, 0, -1):
+        res += N / i
+    return res
+
+def main():
+    N = int(input())
+    print(calc_expectation(N))
+
+if __name__ == "__main__":
+    main()
