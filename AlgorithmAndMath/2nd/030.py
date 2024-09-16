3c19f3ba5485639d441159a1b8272034038e4255
Hiraku Onuma <hiraku00@gmail.com>
2024-09-16 23:14:16 +0900

    add 030(2nd)

---
+++ AlgorithmAndMath/2nd/030.py
@@ -0,0 +1,14 @@
+def knapsack(N, W, items):
+    dp = [0] * (W+1)
+    for w, v in items:
+        for j in range(W, w-1, -1):
+            dp[j] = max(dp[j], dp[j-w]+v)
+    return dp[W]
+
+def main():
+    N, W = map(int, input().split())
+    items = [tuple(map(int, input().split())) for _ in range(N)]
+    print(knapsack(N, W, items))
+
+if __name__ == "__main__":
+    main()
