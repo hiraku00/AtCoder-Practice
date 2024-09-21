dca23af826a9e8cbe4b00418add448948983b17d
Hiraku Onuma <hiraku00@gmail.com>
2024-09-21 14:58:08 +0900

    add 036(2nd)

---
+++ AlgorithmAndMath/2nd/036.py
@@ -0,0 +1,17 @@
+import math
+
+def calc_distance(A, B, H, M):
+    angle_H = (360 * (H / 12)) + (30 * (M / 60))
+    angle_M = 360 * (M / 60)
+    angle_diff = abs(angle_H - angle_M)
+    if angle_diff > 180:
+        angle_diff = 360 - angle_diff
+    angle_rad = math.radians(angle_diff)
+    return math.sqrt(A**2 + B**2 - 2*A*B*math.cos(angle_rad))
+
+def main():
+    A, B, H, M = map(int, input().split())
+    print(calc_distance(A, B, H, M))
+
+if __name__ == "__main__":
+    main()
