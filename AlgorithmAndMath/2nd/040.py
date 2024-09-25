883a6d05aeaad62eece7dcadc4d13b4090e49176
Hiraku Onuma <hiraku00@gmail.com>
2024-09-25 14:27:12 +0900

    add 040(2nd)

---
+++ AlgorithmAndMath/2nd/040.py
@@ -0,0 +1,19 @@
+def calc_total_distance(A, M, B):
+    dist = 0
+    for i in range(M-1):
+        start, end = B[i]-1, B[i+1]-1
+        if start < end:
+            dist += sum(A[start:end])
+        else:
+            dist += sum(A[end:start])
+    return dist
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    M = int(input())
+    B = [int(input()) for _ in range(M)]
+    print(calc_total_distance(A, M, B))
+
+if __name__ == "__main__":
+    main()
