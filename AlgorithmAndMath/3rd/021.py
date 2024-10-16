c537727f56d01338d7dc55d127913163b67d1ff8
Hiraku Onuma <hiraku00@gmail.com>
2024-10-16 12:17:47 +0900

    add 021(3rd)

---
+++ AlgorithmAndMath/3rd/021.py
@@ -0,0 +1,13 @@
+def combination(n, r):
+    r = min(r, n-r)
+    res = 1
+    for i in range(r):
+        res = res * (n-i) // (i+1)
+    return res
+
+def main():
+    n, r = map(int, input().split())
+    print(combination(n, r))
+
+if __name__ == "__main__":
+    main()
