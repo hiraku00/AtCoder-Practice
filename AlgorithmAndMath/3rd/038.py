562aeea2fe56539802673bc7f525818aaf733cb0
Hiraku Onuma <hiraku00@gmail.com>
2024-10-27 11:30:44 +0900

    add 038(3rd)

---
+++ AlgorithmAndMath/3rd/038.py
@@ -0,0 +1,19 @@
+def output(LR, cumulative_sum):
+    for L, R in LR:
+        print(cumulative_sum[R] - cumulative_sum[L-1])
+
+def calc_cumulative_sum(N, A):
+    cumulative_sum = [0] * (N+1)
+    for i in range(N):
+        cumulative_sum[i+1] = cumulative_sum[i] + A[i]
+    return cumulative_sum
+
+def main():
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+    LR = [list(map(int, input().split())) for _ in range(Q)]
+    cumulative_sum = calc_cumulative_sum(N, A)
+    output(LR, cumulative_sum)
+
+if __name__ == "__main__":
+    main()
