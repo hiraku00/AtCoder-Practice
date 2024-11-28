58b882d4db294bb9bd2ee1cc9738bf0a303bc6d4
Hiraku Onuma <hiraku00@gmail.com>
2024-11-28 12:11:10 +0900

    add 029-030(4th)

---
+++ AlgorithmAndMath/4th/029.py
@@ -0,0 +1,13 @@
+def min_cost(N):
+    dp = [0] * (N+1)
+    dp[0] = dp[1] = 1
+    for i in range(2, N+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    return dp[-1]
+
+def main():
+    N = int(input())
+    print(min_cost(N))
+
+if __name__ == "__main__":
+    main()
---
+++ AlgorithmAndMath/4th/030.py
@@ -0,0 +1,19 @@
+def max_value(N, W, items):
+    dp = [0] * (W+1)
+    for w, v in items:
+        print(f'======================== w, v : {w}, {v}')
+        for i in range(W, w-1, -1):
+            print(f'----------------- i : {i}')
+            print(f'dp[{i}]    : {dp[i]}')
+            print(f'dp[{i-w}]    : {dp[i-w]}')
+            print(f'dp[{i-w}]+{v} : {dp[i-w]+v}')
+            dp[i] = max(dp[i], dp[i-w]+v)
+    return dp[-1]
+
+def main():
+    N, W = map(int, input().split())
+    items = [list(map(int, input().split())) for _ in range(N)]
+    print(max_value(N, W, items))
+
+if __name__ == "__main__":
+    main()
